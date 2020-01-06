02529839bacb861166931f36b9701581a86f191b
Lydia1009 <55180772+Lydia1009@users.noreply.github.com>
2020-01-06 23:40:46 +0800

    角色2

---
+++ Unity_106051066_Lydia/Assets/Scripts/CameriaTrack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameriaTrack : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0, 5, 4);//相機相對於玩家的位置
+    private Transform target;
+    private Vector3 pos;
+
+    public float speed = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = target.position + offset;
+        this.transform.position = Vector3.Lerp(this.transform.position, pos, speed * Time.deltaTime);//調整camera與玩家距離
+        Quaternion angel = Quaternion.LookRotation(target.position - this.transform.position);//旋轉角度
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, angel, speed * Time.deltaTime);
+
+    }
+}
